a9c7a3ad3ca2444cbdf8d3401f7ab7e57e3d7313
Porom <porom.kamal@gmail.com>
2025-01-18 13:11:41 -0500

    feat: Added a failsafe for the case that the public API is down

--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// Override fetch
+// window.fetch = async function (...args) {
+//     const response = await originalFetch('/manufacturers.json');
+//     const data = await response.json();
+//     return new Response(JSON.stringify(data), {
+//       status: 200,
+//       headers: { 'Content-Type': 'application/json' },
+//     });
+// };
 root.render(
   <React.StrictMode>
     <App />
